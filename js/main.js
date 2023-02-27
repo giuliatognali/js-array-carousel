ee1511fc6da9c3b8a14c23b286384f4e73d0cbcc
giuliatognali <giuliatognali@gmail.com>
2023-02-27 20:06:01 +0100

    milestone 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//array delle immagini
+const imgs = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
+
+const slider = document.querySelector('.slider');
+let itemsContent = '';
+
+//variabile dell'immagine dell'array
+let active = 0;
+
+for(let i = 0; i < imgs.length; i++){
+    console.log(imgs[i]);
+
+    itemsContent += '<div class="item"><img src="img/01.webp" alt=""></div>'
+}
+
+slider.innerHTML = slider.innerHTML + itemsContent;
+document.querySelector('.item').classList.add('show');
+
+//variabili per l'effetto slider
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+
+//definisco gli eventi 
+
+next.addEventListener('click',
+function(){
+ alert('successivo')
+
+})
+prev.addEventListener('click',
+function(){
+ alert('precedente')
+
+})
+
